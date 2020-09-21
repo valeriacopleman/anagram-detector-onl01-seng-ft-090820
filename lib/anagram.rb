ac9bd9024bcbb6d81dd70956f2a37d6bec9353bf
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-21 03:45:10 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,7 @@ class Anagram
   
   def match(list)
     list.select do |single_word|
-      single_word.chars.sort == @words.char.sort
+      single_word.char.sort == @words.char.sort
   end
 end
 
