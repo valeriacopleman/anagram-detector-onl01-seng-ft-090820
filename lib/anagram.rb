ac25630fd83eed5960c1b7d559cc2545cb721999
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-21 03:46:09 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+  
+  attr_accessor :words
+  
+  def initialize(words)
+    @words = words
+  end
+  
+  def match(list)
+    list.select do |single_word|
+      single_word.chars.sort == @words.chars.sort
+  end
+end
+
+end
