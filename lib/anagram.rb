1984adf5499339e263c5d6a61f666f0b7f08ad71
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-21 03:09:12 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,7 @@ class Anagram
   end
   
   def match(array_of_words)
-    array_of_words= []
+    
       array_of_words.select do |element|
       (@word.split("").sort) == (element.split("").sort)
     end
